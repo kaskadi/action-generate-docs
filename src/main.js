269a7cd767ac1fb9ed1973ea995a7e97f1590fab
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-08-28 19:39:18 +0200

    Updated main job

--- src/main.js
+++ src/main.js
@@ -1,1 +1,6 @@
-console.log('Your action successfully ran!')
+const core = require('@actions/core')
+
+const type = core.getInput('type')
+const templatePath = core.getInput('template')
+
+require(`./helpers/${type}/index.js`)(templatePath)
