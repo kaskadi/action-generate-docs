fefd7b9813d0ace6aa00366367a94afdb9afc1bb
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-08-31 14:38:04 +0200

    Added commit/push logic

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 const core = require('@actions/core')
 
+const test = !process.env.GITHUB_ACTIONS || process.env.GITHUB_REPOSITORY === 'kaskadi/action-generate-docs'
+
 const type = core.getInput('type', { required: true })
 const templatePath = core.getInput('template')
 
 require(`./helpers/${type}/index.js`)(templatePath)
+require('./helpers/push-docs.js')(test)
