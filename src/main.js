8feb4dab1baa73c30ed1fb70224519b150b55b36
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-08-31 12:05:14 +0200

    Added required input error for doc type input

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 const core = require('@actions/core')
 
-const type = core.getInput('type')
+const type = core.getInput('type', { required: true })
 const templatePath = core.getInput('template')
 
 require(`./helpers/${type}/index.js`)(templatePath)
