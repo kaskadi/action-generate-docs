ddd69ddfe981031c02e76a34d8445ffad9155fad
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-08-31 15:19:17 +0200

    Added logging to give user feedback

--- src/helpers/action/index.js
+++ src/helpers/action/index.js
@@ -1,7 +1,11 @@
 const fs = require('fs')
 
 module.exports = templatePath => {
+  console.log('INFO: reading action configuration file...')
   const config = require('./parse-config.js')(fs)
+  console.log('SUCCESS: extracted action configuration!')
+  console.log('INFO: generating documentation...')
   const docs = require('./build-docs.js')(fs, config, templatePath)
+  console.log('SUCCESS: documentation successfully generated!')
   fs.writeFileSync('README.md', docs, 'utf8')
 }
