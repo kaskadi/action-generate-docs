e7aa14474541a5ab84c4226eee6fed4dfde37a7d
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-09-02 11:48:24 +0200

    Generated documentation

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 const core = require('@actions/core')
+const markshell = require('markshell')
 
 const test = process.env.TEST_ENV ? JSON.parse(process.env.TEST_ENV) : false
 
@@ -6,4 +7,11 @@ const type = core.getInput('type', { required: true })
 const templatePath = core.getInput('template')
 
 require(`./helpers/${type}/index.js`)(templatePath)
+try {
+  console.log('New documentation preview:')
+  markshell.toConsole('README.md')
+} catch (err) {
+  console.log('ERROR: was not able to generate new documentation preview... This shouldn\'t impact the actual documentation.')
+  console.log(err)
+}
 require('./helpers/push-docs.js')(test)
