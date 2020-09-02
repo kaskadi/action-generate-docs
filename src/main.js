7c38b572e26593d2118b65f93010457b0655cc13
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-09-02 13:04:53 +0200

    Upgraded markshell + improved user feedback

--- src/main.js
+++ src/main.js
@@ -9,8 +9,9 @@ const templatePath = core.getInput('template')
 
 require(`./helpers/${type}/index.js`)(templatePath)
 try {
-  console.log('New documentation preview:')
+  console.log('\n********* Documentation preview *********\n')
   markshell.toConsole(path.join(__dirname, '..', 'README.md'))
+  console.log('\n********* END Documentation preview *********\n')
 } catch (err) {
   console.log('ERROR: was not able to generate new documentation preview... This shouldn\'t impact the actual documentation.')
   console.log(err)
