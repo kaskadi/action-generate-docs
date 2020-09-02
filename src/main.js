a40d70f12d848b58baeabec81d123c51bbfd228f
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-09-02 13:37:28 +0200

    Fixed path to documentation for docs preview

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 const core = require('@actions/core')
 const markshell = require('markshell')
-const path = require('path')
 
 const test = process.env.TEST_ENV ? JSON.parse(process.env.TEST_ENV) : false
 
@@ -10,7 +9,7 @@ const templatePath = core.getInput('template')
 require(`./helpers/${type}/index.js`)(templatePath)
 try {
   console.log('\n********* Documentation preview *********\n')
-  markshell.toConsole(path.join(__dirname, '..', 'README.md'))
+  markshell.toConsole('README.md')
   console.log('\n********* END Documentation preview *********\n')
 } catch (err) {
   console.log('ERROR: was not able to generate new documentation preview... This shouldn\'t impact the actual documentation.')
