fd8f9eb424f0867aae927673a1f6bf71253b1859
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-09-15 16:52:49 +0200

    Fixed package.json loading

--- src/helpers/package/get-main.js
+++ src/helpers/package/get-main.js
@@ -1,14 +1,14 @@
 const jsdoc2md = require('jsdoc-to-markdown')
 
 module.exports = ({ replaceInFile, fs, path }) => {
-  const pjson = require('./package.json')
+  const pjson = JSON.parse(fs.readFileSync('package.json', 'utf8'))
   const replaceOpts = {
     flags: pjson.bin ? '-g' : '',
     'repo-name': pjson.name,
     main: genBaseDocs()
   }
   let main = fs.readFileSync(path.join(__dirname, 'template.md'), 'utf8')
-  for (const key of replaceOpts) {
+  for (const key in replaceOpts) {
     main = replaceInFile(main, key, replaceOpts[key])
   }
   return main
