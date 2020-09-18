d93100ecef7955a23e7f96cfb48e9b2e945a1042
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-09-18 12:00:27 +0200

    Added test for invalid type input

--- test/action.test.js
+++ test/action.test.js
@@ -1,10 +1,18 @@
 /* eslint-env mocha */
+const runAction = require('./helpers/run-action.js')
+const { existsSync } = require('fs')
+const chai = require('chai')
+chai.should()
 
 describe('template-action', function () {
   // ******* DO NOT REMOVE THIS TEST!
   require('./pre/tests.js')
   // *******
-
+  it('should error when using a non supported repository type', async function () {
+    process.env.INPUT_TYPE = 'hello'
+    await runAction(['main'])
+    existsSync('test/README.md').should.equal(false)
+  })
   require('./action/tests.js')
   require('./package/tests.js')
 })
