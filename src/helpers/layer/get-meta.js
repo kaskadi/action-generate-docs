2a97cba63e2b24c7c429dcf75a379ae61a4671ea
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-09-22 15:18:38 +0200

    Added error handling for non existing config file

--- src/helpers/layer/get-meta.js
+++ src/helpers/layer/get-meta.js
@@ -5,7 +5,7 @@ const snakeToCamel = require('../snake-to-camel.js')
 module.exports = ({ fs }) => {
   const { name, description } = JSON.parse(fs.readFileSync('package.json', 'utf8'))
   const layerName = snakeToCamel(name)
-  const sls = getSls(YAML, __dirname)
+  const sls = getSls(YAML, fs, __dirname)
   const layerPath = sls.layers[layerName].path
   return {
     description: description.length > 0 ? description : 'No description found in package.json...',
