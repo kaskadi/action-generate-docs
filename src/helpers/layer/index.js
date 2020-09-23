d12439e6ae0a5644c3d838b50b02c365c65cc308
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-09-23 13:13:07 +0200

    Fixed case when no layer is defined

--- src/helpers/layer/index.js
+++ src/helpers/layer/index.js
@@ -7,7 +7,7 @@ const modules = {
 module.exports = templatePath => {
   console.log('INFO: retrieving layers data from serverless.yml file...')
   const layersData = require('../get-sls.js')(modules, __dirname).layers
-  const layers = Object.values(layersData)
+  const layers = Object.values(layersData || {})
   console.log('SUCCESS: extracted layer meta data!')
   const data = require('./get-packages.js')(modules, layers)
   console.log('INFO: generating documentation...')
