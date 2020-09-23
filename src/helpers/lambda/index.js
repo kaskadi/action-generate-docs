f4476bebf52f8ba87a0eafd4a86c868e2e29b626
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-09-23 13:52:25 +0200

    Code cleanup

--- src/helpers/lambda/index.js
+++ src/helpers/lambda/index.js
@@ -7,9 +7,11 @@ const modules = {
 module.exports = templatePath => {
   console.log('INFO: retrieving lambda functions data from serverless.yml file...')
   const meta = require('../get-sls.js')(modules, __dirname)
+  const functions = meta.functions || {}
+  const layers = meta.layers || {}
   const data = {
-    functions: require('./process-meta.js')(Object.values(meta.functions, meta.layers)),
-    layers: require('../layer/get-packages.js')(modules, Object.values(meta.layers || {}))
+    functions: require('./process-meta.js')(Object.values(functions, layers)),
+    layers: require('../layer/get-packages.js')(modules, Object.values(layers))
   }
   console.log('SUCCESS: extracted lambda functions meta data!')
   console.log('INFO: generating documentation...')
