18a72724521e0bc8dd5bcecc2ddf4c1ece43cb88
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-09-23 13:12:36 +0200

    Empty list style updated

--- src/helpers/build-list.js
+++ src/helpers/build-list.js
@@ -1,4 +1,4 @@
 // used for building list from layers and lambda functions metadata
 module.exports = (arr, docType) => {
-  return arr.length > 0 ? arr.map(data => `- [${data.name}](#${data.name})`).join('\n') : `No ${docType} defined in the configuration file...`
+  return arr.length > 0 ? arr.map(data => `- [${data.name}](#${data.name})`).join('\n') : `_no ${docType} defined in the configuration file..._`
 }
