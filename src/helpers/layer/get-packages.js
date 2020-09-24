259ff100a763df705d2b0508e0ff0977ed3ebfb2
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-09-24 13:59:47 +0200

    Implemented base logic for local pkg retrieval

--- src/helpers/layer/get-packages.js
+++ src/helpers/layer/get-packages.js
@@ -1,18 +1,54 @@
-module.exports = ({ fs }, layers) => {
+module.exports = ({ fs, path }, layers) => {
   return layers.map(layer => {
-    const dependencies = getDeps(fs, layer.path)
+    const dependencies = getDeps(fs, path, layer.path)
     return {
       ...layer,
-      dependencies: dependencies.length > 0 ? buildDepList(dependencies) : 'No NPM packages installed...'
+      dependencies: dependencies.length > 0 ? buildDepList(dependencies) : 'No dependencies (NPM or local) found...'
     }
   })
 }
 
 function buildDepList (dependencies) {
-  return dependencies.map(dependency => `- \`${dependency}\` ([see on NPM](https://www.npmjs.com/package/${dependency}))`).join('\n')
+  const formatDep = dependency => dependency.type === 'npm'
+    ? `- \`${dependency.name}\`, version: \`${dependency.version}\` ([see on NPM](https://www.npmjs.com/package/${dependency.name}))`
+    : `- \`${dependency.name}\` (local utility)`
+  return dependencies.map(formatDep).join('\n')
 }
 
-function getDeps (fs, layerPath) {
-  const { dependencies } = JSON.parse(fs.readFileSync(`${layerPath}/nodejs/package.json`, 'utf8'))
-  return dependencies ? Object.keys(dependencies) : []
+function getDeps (fs, path, layerPath) {
+  const cwd = process.cwd()
+  process.chdir(path.join(cwd, `${layerPath}/nodejs`))
+  const pjson = JSON.parse(fs.readFileSync('package.json', 'utf8'))
+  const dependencies = pjson.dependencies || {}
+  const npmDeps = Object.entries(dependencies).map(getNpmDepData)
+  const localDeps = getLocalDeps(fs, pjson)
+  process.chdir(cwd)
+  return [...npmDeps, ...localDeps]
+}
+
+function getNpmDepData (dep) {
+  return {
+    name: dep[0],
+    version: dep[1],
+    type: 'npm'
+  }
+}
+
+function getLocalDeps (fs, pjson) {
+  const { spawnSync } = require('child_process')
+  const tempPjson = { ...pjson }
+  delete tempPjson.dependencies
+  fs.writeFileSync('package.json', JSON.stringify(tempPjson, null, 2), 'utf8')
+  spawnSync('npm', ['i', '--only=prod'])
+  fs.writeFileSync('package.json', JSON.stringify(pjson, null, 2), 'utf8')
+  return fs.readdirSync('node_modules', { withFileTypes: true })
+    .filter(dirent => dirent.isDirectory())
+    .map(dirent => dirent.name)
+    .filter(module => module !== '.bin')
+    .map(module => {
+      return {
+        name: module,
+        type: 'local'
+      }
+    })
 }
