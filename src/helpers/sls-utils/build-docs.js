383d13ab75aad507508ef6893fec69bb5e0a5bca
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-09-29 15:27:46 +0200

    Simplified code logic

--- src/helpers/sls-utils/build-docs.js
+++ src/helpers/sls-utils/build-docs.js
@@ -1,12 +1,10 @@
-const { getPartial, buildList } = require('./utils.js')
 const fs = require('fs')
 const path = require('path')
+const getPartials = require('./get-partials.js')
 const replaceInFile = require('../replace-in-file.js')
 
-const srcDir = path.join(__dirname, '../../')
-
 module.exports = (data, templatePath, type) => {
-  let main = fs.readFileSync(path.join(srcDir, `main-handlers/${type}/main-partial.md`), 'utf8')
+  let main = fs.readFileSync(path.join(__dirname, `../../main-handlers/${type}/main-partial.md`), 'utf8')
   const handlers = {
     layer: getLayersData,
     lambda: getLambdasData,
@@ -25,45 +23,30 @@ module.exports = (data, templatePath, type) => {
 
 function getLayersData (data) {
   const { layers } = data
-  const layerDocType = 'layer'
-  const partialPath = path.join(srcDir, 'main-handlers/layer/layer-partial.md')
-  return {
-    layers: getPartial(fs, layers, partialPath, layerDocType),
-    'layers-list': buildList(layers, layerDocType)
-  }
+  return getPartials(layers, 'layers')
 }
 
 function getLambdasData (data) {
   const addDetails = require('../../main-handlers/lambda/add-details.js')
   const { functions } = data
-  const lambdaDocType = 'lambda function'
-  const partialPath = path.join(srcDir, 'main-handlers/lambda/lambda-partial.md')
   const lambdas = functions.map(addDetails(fs, path))
   return {
     ...getLayersData(data),
-    lambdas: getPartial(fs, lambdas, partialPath, lambdaDocType),
-    'lambdas-list': buildList(lambdas, lambdaDocType)
+    ...getPartials(lambdas, 'lambdas')
   }
 }
 
 function getEndpointsData (data) {
   const buildMethods = require('../../main-handlers/api/build-methods.js')
   let { endpoints } = data
-  const endpointDocType = 'endpoint'
-  const methodDocType = 'method'
-  const methodPartialPath = path.join(srcDir, 'main-handlers/api/method-partial.md')
-  const endpointPartialPath = path.join(srcDir, 'main-handlers/api/endpoint-partial.md')
-  endpoints = buildMethods(endpoints)
-  endpoints = endpoints.map(endpoint => {
+  endpoints = buildMethods(endpoints).map(endpoint => {
     return {
       ...endpoint,
-      'methods-list': buildList(endpoint.methods, methodDocType),
-      methods: getPartial(fs, endpoint.methods, methodPartialPath, methodDocType)
+      ...getPartials(endpoint.methods, 'methods')
     }
   })
   return {
     ...getLambdasData(data),
-    endpoints: getPartial(fs, endpoints, endpointPartialPath, endpointDocType),
-    'endpoints-list': buildList(endpoints, endpointDocType)
+    ...getPartials(endpoints, 'endpoints')
   }
 }
---
+++ src/helpers/sls-utils/get-partials.js
@@ -0,0 +1,46 @@
+const fs = require('fs')
+const path = require('path')
+
+const handlersDir = path.join(__dirname, '../../main-handlers')
+const docTypes = {
+  layers: {
+    docType: 'layer',
+    partialPath: path.join(handlersDir, 'layer/layer-partial.md')
+  },
+  lambdas: {
+    docType: 'lambda function',
+    partialPath: path.join(handlersDir, 'lambda/lambda-partial.md')
+  },
+  methods: {
+    docType: 'method',
+    partialPath: path.join(handlersDir, 'api/method-partial.md')
+  },
+  endpoints: {
+    docType: 'endpoint',
+    partialPath: path.join(handlersDir, 'api/endpoint-partial.md')
+  }
+}
+
+module.exports = (data, type) => {
+  var partials = {}
+  const { docType, partialPath } = docTypes[type]
+  partials[type] = getPartial(data, partialPath, docType)
+  partials[`${type}-list`] = buildList(data, docType)
+  return partials
+}
+
+function buildPartial (partialDoc, docType) {
+  const replaceInFile = require('../replace-in-file.js')
+  return data => Object.keys(data).reduce((partial, key) => replaceInFile(partial, key, data[key] || `No ${key} found for this ${docType}...`), partialDoc)
+}
+
+function getPartial (data, partialPath, docType) {
+  const partial = fs.readFileSync(partialPath, 'utf8')
+  return data.map(buildPartial(partial, docType)).join('\n\n').trim()
+}
+
+function buildList (arr, docType) {
+  return arr.length > 0
+    ? arr.map(data => `- [${data.name}](#${data.name})`).join('\n')
+    : `_no ${docType} defined in the configuration file..._`
+}
