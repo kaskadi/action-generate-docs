74b83b640e6f2dd94ccc00f567d93e8ccf353e94
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-10-02 12:24:37 +0200

    Outputting proper sentence case (no upper cases)

--- src/helpers/camel-to-sentence.js
+++ src/helpers/camel-to-sentence.js
@@ -1,4 +1,7 @@
 module.exports = str => {
   const sentenceCasedStr = str.replace(/([a-z])([A-Z])/g, '$1 $2')
-  return sentenceCasedStr.charAt(0).toUpperCase() + sentenceCasedStr.slice(1)
+  return sentenceCasedStr
+    .split(' ')
+    .map(fragment => fragment.toLowerCase())
+    .join(' ')
 }
