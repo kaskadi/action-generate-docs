3d0f9fc02130a3b1ddb2d9c36299d701f81baad3
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-10-02 12:46:00 +0200

    Updated no packages message for layer

--- src/main-handlers/layer/get-packages.js
+++ src/main-handlers/layer/get-packages.js
@@ -5,7 +5,7 @@ module.exports = ({ fs, path }, meta) => {
     return {
       description: '', // default description if none provided
       ...layer,
-      dependencies: dependencies.length > 0 ? buildDepList(dependencies) : 'No NPM packages or local utilities found for this layer...'
+      dependencies: dependencies.length > 0 ? buildDepList(dependencies) : 'No NPM packages or local utilities found for this layer.'
     }
   })
 }
