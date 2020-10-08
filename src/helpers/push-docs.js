7412f6c8530407ea24310581275ce63a15fc7b0a
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-10-08 12:07:22 +0200

    Refactored push-docs to clean up code

--- src/helpers/push-docs.js
+++ src/helpers/push-docs.js
@@ -2,13 +2,20 @@ const { spawnSync } = require('child_process')
 
 module.exports = () => {
   console.log('INFO: commiting and pushing new documentation...')
-  spawnSync('git', ['add', 'README.md'], { stdio: 'inherit' })
-  const gpgSign = spawnSync('git', ['config', 'commit.gpgSign']).stdout
-  let commitParams = ['commit', '-m', 'Generated documentation']
-  commitParams = gpgSign.length > 0 ? addFlag(commitParams, '-S') : commitParams
-  spawnSync('git', commitParams, { stdio: 'inherit' })
-  spawnSync('git', ['push'], { stdio: 'inherit' })
+  spawnGit(['add', 'README.md'])
+  spawnGit(getCommitArgs())
+  spawnGit(['push'])
   console.log('SUCCESS: commited and pushed new documentation!')
+}
+
+function spawnGit (args) {
+  spawnSync('git', args, { stdio: 'inherit' })
+}
+
+function getCommitArgs () {
+  const gpgSign = spawnSync('git', ['config', 'commit.gpgSign']).stdout
+  const commitArgs = ['commit', '-m', 'Generated documentation']
+  return gpgSign.length > 0 ? addFlag(commitArgs, '-S') : commitArgs
 }
 
 function addFlag (args, flag) {
