dcfe82e7eaa157b72675867716d180079f869222
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-10-08 12:03:13 +0200

    Removed dry runs to limit complexity

--- src/helpers/push-docs.js
+++ src/helpers/push-docs.js
@@ -1,18 +1,13 @@
 const { spawnSync } = require('child_process')
 
-module.exports = test => {
+module.exports = () => {
   console.log('INFO: commiting and pushing new documentation...')
-  let stageParams = ['add', 'README.md']
-  stageParams = test ? addFlag(stageParams, '--dry-run') : stageParams
-  spawnSync('git', stageParams, { stdio: 'inherit' })
+  spawnSync('git', ['add', 'README.md'], { stdio: 'inherit' })
   const gpgSign = spawnSync('git', ['config', 'commit.gpgSign']).stdout
   let commitParams = ['commit', '-m', 'Generated documentation']
   commitParams = gpgSign.length > 0 ? addFlag(commitParams, '-S') : commitParams
-  commitParams = test ? addFlag(commitParams, '--dry-run') : commitParams
   spawnSync('git', commitParams, { stdio: 'inherit' })
-  let pushParams = ['push']
-  pushParams = test ? addFlag(pushParams, '--dry-run') : pushParams
-  spawnSync('git', pushParams, { stdio: 'inherit' })
+  spawnSync('git', ['push'], { stdio: 'inherit' })
   console.log('SUCCESS: commited and pushed new documentation!')
 }
 
