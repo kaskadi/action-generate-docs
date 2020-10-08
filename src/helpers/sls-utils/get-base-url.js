b9fbb5c5322cd1096ed03c095ebf33897ae75908
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-10-08 18:04:14 +0200

    Added placeholder when hostname is missing

--- src/helpers/sls-utils/get-base-url.js
+++ src/helpers/sls-utils/get-base-url.js
@@ -2,7 +2,7 @@ module.exports = meta => {
   const custom = meta.custom || {}
   const kaskadiDocs = custom['kaskadi-docs'] || {}
   const { hostname, root } = kaskadiDocs
-  const origin = hostname && hostname.length > 0 ? `https://${hostname}` : ''
+  const origin = hostname && hostname.length > 0 ? `https://${hostname}` : 'https://API_DOMAIN'
   const path = root && root.length > 0 ? `/${root}` : ''
   return origin + path
 }
