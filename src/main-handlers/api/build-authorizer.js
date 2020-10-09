d202601f83a12b273e5e018a017e49aa8ef59fac
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-10-09 15:07:53 +0200

    Moved authorizer data formatting to build step

--- src/main-handlers/api/build-authorizer.js
+++ src/main-handlers/api/build-authorizer.js
@@ -4,15 +4,17 @@ module.exports = ({ table }, authorizerData) => {
   if (Object.keys(authorizerData).length === 0) {
     return ''
   }
+  const data = { ...authorizerData }
+  data.identitySource = `<ul>\n${data.identitySource.split(', ').map(source => `<li>${source}</li>`).join('\n')}\n</ul>`
   return table(
-    Object.entries(authorizerData)
+    Object.entries(data)
       .map(entry => {
         let key = camelToSentence(entry[0])
         key = key.charAt(0).toUpperCase() + key.slice(1)
         return [key, entry[1]]
       }),
     {
-      align: Object.keys(authorizerData)
+      align: Object.keys(data)
         .map(key => key === 'identitySource' ? 'l' : 'c')
     }
   )
