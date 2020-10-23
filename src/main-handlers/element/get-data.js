204a74a0e4d669cb20339294b8c44ac4d1304aef
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-10-23 14:13:26 +0200

    Handling absence of publish config or match

--- src/main-handlers/element/get-data.js
+++ src/main-handlers/element/get-data.js
@@ -1,17 +1,19 @@
 module.exports = ({ fs }) => {
   const pjson = JSON.parse(fs.readFileSync('package.json', 'utf8'))
   // TODO: below is my original approach for the code. This works only in Node 14+ and Node 14 will move to LTS on 27.10.2020 with Node 12 going to maintenance on 30.11.2020. This means we won't be using this syntax for now but may want to enable node14 in the action at some point in the future
-  // const uploadData = pjson?.kaskadi?.['s3-push']?.files
-  const uploadData = pjson.kaskadi
+  // const files = pjson?.kaskadi?.['s3-push']?.files || []
+  const files = pjson.kaskadi
     ? pjson.kaskadi['s3-push']
-      ? pjson.kaskadi['s3-push'].files
-      : undefined
-    : undefined
-  const path = uploadData
-    ? uploadData
-      .filter(data => data.dest.includes(pjson.main))[0]
-      .dest
-    : ''
+      ? pjson.kaskadi['s3-push'].files || []
+      : []
+    : []
+  const matchingFiles = files.filter(file => file.dest.includes(pjson.main))
+  if (matchingFiles.length === 0) {
+    return {
+      instructions: 'Usage instructions unavailable: no published files were matching the _main_ file provided in [`package.json`](./package.json). You may want to have a look at the definition of `kaskadi.s3-push.files` custom field definition in [`package.json`](./package.json).'
+    }
+  }
+  const path = matchingFiles[0].dest
   const paths = {
     masterPath: transformPath(path),
     releasePath: transformPath(path, 'release/v1.0.0/')
