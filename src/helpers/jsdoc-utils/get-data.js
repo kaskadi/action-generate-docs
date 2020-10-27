de09812e998875fef71c855cde423f3ffc72ebfe
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-10-27 16:19:15 +0100

    Added handling for non existing file

--- src/helpers/jsdoc-utils/get-data.js
+++ src/helpers/jsdoc-utils/get-data.js
@@ -1,17 +1,25 @@
 const { spawnSync } = require('child_process')
 
 module.exports = ({ fs, glob, jsdoc2md }, type) => {
-  const opts = {
-    files: type === 'package'
-      ? glob.sync('**/*.js', { ignore: 'node_modules/**' })
-      : JSON.parse(fs.readFileSync('package.json', 'utf8')).main,
-    'example-lang': type === 'package'
-      ? 'js'
-      : 'html',
+  let opts = {
+    'example-lang': type === 'package' ? 'js' : 'html',
     'no-cache': true,
     'heading-depth': 2,
     plugin: '@godaddy/dmd'
   }
+  let files
+  if (type !== 'package') {
+    const { main } = JSON.parse(fs.readFileSync('package.json', 'utf8'))
+    if (!fs.existsSync(main)) {
+      return {
+        main: `No file is matching the main file (\`${main}\`) provided in \`package.json\`...`
+      }
+    }
+    files = main
+  } else {
+    files = glob.sync('**/*.js', { ignore: 'node_modules/**' })
+  }
+  opts = { ...opts, files }
   spawnSync('npm', ['i', '@godaddy/dmd'])
   const main = jsdoc2md.renderSync(opts).trim()
   spawnSync('npm', ['rm', '@godaddy/dmd'])
