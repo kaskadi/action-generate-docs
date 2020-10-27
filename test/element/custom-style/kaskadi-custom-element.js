729ff2815164e6a1d7a3574f9a62ab04babf809c
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-10-27 16:20:13 +0100

    Added test for CSS custom props

---
+++ test/element/custom-style/kaskadi-custom-element.js
@@ -0,0 +1,51 @@
+/* eslint-env browser, mocha */
+import { KaskadiElement, html, css } from 'https://cdn.klimapartner.net/modules/@kaskadi/kaskadi-element/kaskadi-element.js'
+
+/**
+ * Template element for the Kaskadi application
+ *
+ * @module kaskadi-custom-element
+ *
+ * @param {string} lang - element's language
+ * @param {string} title - element's title
+ *
+ * @example
+ *
+ * <kaskadi-custom-element title="Welcome!" lang="en"></kaskadi-custom-element>
+ */
+
+class KaskadiCustomElement extends KaskadiElement {
+  static get properties () {
+    return {
+      lang: { type: String },
+      title: { type: String }
+    }
+  }
+
+  // this styles comes from kaskadi-date-icon. It doesn't make much sense here but it's for test only
+  static get styles () {
+    return css`
+      :host, svg{
+        width:var(--icon-size, 48px);
+        height:var(--icon-size, 48px);
+        display: inline-block;
+      }
+      #bg { fill: var(--background-color, white) }
+      #outline { stroke: var(--outline-color, #333) }
+      #head { fill: var(--head-color, royalblue) }
+      #day { fill: var(--day-color, var(--outline-color, #333)) }
+      #monat { fill: var(--month-color, white) }
+      #name { fill: var(--name-color, var(--outline-color, #333)) }
+
+    `
+  }
+
+  render () {
+    return html`
+      <h1>${this.title}</h1>
+      <p>Current language is ${this.lang}</p>
+    `
+  }
+}
+
+customElements.define('kaskadi-custom-element', KaskadiCustomElement)
---
+++ test/element/custom-style/validation.md
@@ -0,0 +1,47 @@
+# Usage instructions
+
+In another element:
+```js
+// using the latest version
+import 'https://cdn.klimapartner.net/modules/@kaskadi/kaskadi-custom-element/kaskadi-custom-element.js'
+// using a specific version
+import 'https://cdn.klimapartner.net/modules/@kaskadi/kaskadi-custom-element/release/v1.0.0/kaskadi-custom-element.js'
+```
+
+In the browser:
+```html
+<!-- using the latest version -->
+<script type="module" src="https://cdn.klimapartner.net/modules/@kaskadi/kaskadi-custom-element/kaskadi-custom-element.js"></script>
+<!-- using a specific version -->
+<script type="module" src="https://cdn.klimapartner.net/modules/@kaskadi/kaskadi-custom-element/release/v1.0.0/kaskadi-custom-element.js"></script>
+```
+
+# Custom element documentation
+
+## kaskadi-custom-element
+
+Template element for the Kaskadi application
+
+
+| Param | Type | Description |
+| --- | --- | --- |
+| lang | `string` | element's language |
+| title | `string` | element's title |
+
+**Example**  
+```html
+<kaskadi-custom-element title="Welcome!" lang="en"></kaskadi-custom-element>
+```
+<!-- LINKS -->
+
+## Custom styles
+
+The following custom CSS properties are available for this element:
+
+- `--icon-size`
+- `--background-color`
+- `--outline-color`
+- `--head-color`
+- `--day-color`
+- `--month-color`
+- `--name-color`
