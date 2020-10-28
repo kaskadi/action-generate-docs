5e2664fd52ae13f34f9378730c9be354e86f251f
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-10-28 18:13:54 +0100

    Added ability to specify an example language

--- src/helpers/jsdoc-utils/get-data.js
+++ src/helpers/jsdoc-utils/get-data.js
@@ -1,27 +1,42 @@
 const { spawnSync } = require('child_process')
 
-module.exports = ({ fs, glob, jsdoc2md }, type) => {
+module.exports = (modules, type) => {
+  const pjson = require(`${process.cwd()}/package.json`)
   let opts = {
-    'example-lang': type === 'package' ? 'js' : 'html',
+    'example-lang': getExampleLang(pjson, type),
     'no-cache': true,
     'heading-depth': 2,
     plugin: '@godaddy/dmd'
   }
-  let files
-  if (type !== 'package') {
-    const { main } = require(`${process.cwd()}/package.json`)
-    if (!fs.existsSync(main)) {
-      return {
-        main: `No file is matching the main file (\`${main}\`) provided in \`package.json\`...`
-      }
+  const files = getFiles(modules, pjson, type)
+  if (files.length === 0) {
+    return {
+      main: `No file is matching the main file (\`${pjson.main}\`) provided in \`package.json\`...`
     }
-    files = main
-  } else {
-    files = glob.sync('**/*.js', { ignore: 'node_modules/**' })
   }
   opts = { ...opts, files }
   spawnSync('npm', ['i', '@godaddy/dmd'])
+  const { jsdoc2md } = modules
   const main = jsdoc2md.renderSync(opts).trim()
   spawnSync('npm', ['rm', '@godaddy/dmd'])
   return { main }
+}
+
+function getExampleLang (pjson, type) {
+  const { kaskadi } = pjson
+  if (kaskadi && kaskadi.docs && kaskadi.docs['example-lang']) {
+    return kaskadi.docs['example-lang']
+  }
+  return type === 'package' ? 'js' : 'html'
+}
+
+function getFiles ({ fs, glob }, pjson, type) {
+  if (type !== 'package') {
+    const { main } = pjson
+    if (!fs.existsSync(main)) {
+      return ''
+    }
+    return main
+  }
+  return glob.sync('**/*.js', { ignore: 'node_modules/**' })
 }
