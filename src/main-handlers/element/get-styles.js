d87c4462d6176a234f8c16629d2f871f61463cd9
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-10-30 12:56:05 +0100

    Updated matching logic for CSS props retrieval

--- src/main-handlers/element/get-styles.js
+++ src/main-handlers/element/get-styles.js
@@ -8,10 +8,34 @@ module.exports = (fs, main) => {
 function getCustomVars (fs, main) {
   const { readFileSync } = fs
   const elem = readFileSync(main, 'utf8')
-  const customVarsRegex = new RegExp(/var\((.[^,]+),{1}.+\){1}/, 'g') // with this regexp we match var(...,...) and capture the first parameter (everything before comma, comma excluded => .[^,]+ )
+  const customVars = findCustomVars(elem)
+  return filterCustomVars(customVars)
+}
+
+function findCustomVars (elem) {
+  const customVarsRegex = new RegExp(/(--.[^:,)]+)(?:(:)(.[^;)]+))?/, 'g')
+  // Regexp explanation:
+  // - (--.[^:,)]+): have a capture group which matches a custom property (-- followed by any characters except colon and comma)
+  // - (?:(:)(.[^;)]+))?: we match up to 1 time a group consisting of a colon followed by anything which is not a semi-colon or a closing round bracket. The only part captured from this match is everything after the colon
   const matches = Array.from(elem.matchAll(customVarsRegex)) // we use here matchAll because we do a global match and unfortunately .match() doesn't return the individual capture groups. .matchAll() returns an iteratble, so we have to turn it into an Array
-  const customVars = matches.map(match => match[1].trim())
-  return [...new Set(customVars)]
+  return matches.map(match => {
+    const name = match[1]
+    const value = match[3]
+    return {
+      name: name.trim(),
+      ...(value && { default: value.trim() })
+    }
+  })
+}
+
+function filterCustomVars (customVars) {
+  const uniqueVars = [...new Set(customVars.map(variable => variable.name))]
+  console.log(uniqueVars)
+  return uniqueVars.map(variable => {
+    const matchingVars = customVars.filter(customVariable => customVariable.name === variable)
+    const matchingVarsWithDefault = matchingVars.filter(customVariable => customVariable.default)
+    return matchingVarsWithDefault.length > 0 ? matchingVarsWithDefault[0] : matchingVars[0] // we grab here only the first variable with default found (if there is one) because defining multiple default is bad practice
+  })
 }
 
 function buildStylesDocs (cssVars) {
