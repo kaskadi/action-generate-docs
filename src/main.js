7a85f7e1775b762cf5b4920d2f0e461eb3dc69b1
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-12-02 14:15:32 +0100

    Added grouping for logs

--- src/main.js
+++ src/main.js
@@ -1,21 +1,25 @@
-const core = require('@actions/core')
+const { getInput, startGroup, endGroup, warning } = require('@actions/core')
 const path = require('path')
 const fs = require('fs')
 
-const test = process.env.TEST_ENV ? JSON.parse(process.env.TEST_ENV) : false
-
-const type = core.getInput('type', { required: true })
-const templatePath = core.getInput('template')
+const type = getInput('type', { required: true })
+const templatePath = getInput('template')
 
 if (!fs.existsSync(`${__dirname}/main-handlers/${type}`)) {
-  console.log(`WARNING: ${type} is not a supported repository type... Please see action documentation for more information. Aborting job...`)
+  const warningMsg = `${type} is not a supported repository type... Please see action documentation for more information.`
+  warning(warningMsg)
+  console.log(`WARNING: ${warningMsg} Aborting job...`)
   process.exit(0)
 }
 
 const modulePath = `main-handlers/${type}`
 
+startGroup(`Installing ${type} documentation generation module dependencies`)
 require('./helpers/install-deps.js')(path.join(__dirname, modulePath))
+endGroup()
+startGroup('Generating documentation for repository')
 require(`./${modulePath}/index.js`)(templatePath)
-if (!test) {
-  require('./helpers/push-docs.js')()
-}
+endGroup()
+startGroup('Adding new documentation to repository')
+require('./helpers/push-docs.js')()
+endGroup()
