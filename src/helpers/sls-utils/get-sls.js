ebe35064084d63ff445afaf7329295590e6c6567
Alexis Lemaire <a.lemaire@klimapartner.de>
2021-02-23 15:50:36 +0100

    Upgraded serverless to latest version + fixed bug

--- src/helpers/sls-utils/get-sls.js
+++ src/helpers/sls-utils/get-sls.js
@@ -5,6 +5,8 @@ module.exports = ({ YAML, fs, slsCli }) => {
     console.log('ERROR: no serverless.yml configuration file found, aborting...')
     process.exit(0)
   }
+  process.env.SLS_WARNING_DISABLE = '*'
+  process.env.SLS_DEPRECATION_DISABLE = '*'
   const sls = spawnSync('node', [slsCli, 'print']).stdout.toString().trim()
   return YAML.parse(sls)
 }
